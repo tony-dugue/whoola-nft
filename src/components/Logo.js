90f883a5cc2d165e08e6fb25474859f1684f3368
tony dugué <duguetony@gmail.com>
2022-04-18 20:58:10 +0200

    add Logo component

---
+++ src/components/Logo.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Link } from "react-router-dom";
+
+const Logo = () => {
+  return (
+   <LogoText>
+     <Link to="/">W.</Link>
+   </LogoText>
+  )
+}
+
+export default Logo
+
+const LogoText = styled.h1`
+  font-family: 'Akaya Telivigala', cursive;
+  font-size: ${props => props.theme.fontxxxl};
+  color: ${props => props.theme.primary};
+  transition: all 0.2s ease;
+  
+  &:hover {
+    transform: scale(1.1);
+  }
+`
+
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import Logo from "./Logo";
+
 const Navigation = () => {
   return (
    <NavigationWrapper>
      <NavBar>
-       <h2>Logo</h2>
+       <Logo />
        <h2>Menu</h2>
        <h2>Button</h2>
      </NavBar>
