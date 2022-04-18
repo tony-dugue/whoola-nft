6ab1998b6df797e26a65c966e145f6a3b8918cb2
tony dugué <duguetony@gmail.com>
2022-04-18 18:48:37 +0200

    add assets / favicons / fonts / GlobalStyles / ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import GlobalStyles from "./styles/GlobalStyles";
+import {ThemeProvider} from "styled-components";
+import {light} from "./styles/Themes";
 
 function App() {
   return (
     <>
-      App File
+      <GlobalStyles />
+      <ThemeProvider theme={light}>
+        App File
+      </ThemeProvider>
     </>
   );
 }
 
 export default App;
+
---
+++ src/styles/Themes.js
@@ -0,0 +1,49 @@
+// this file contains variables for different themes
+
+export const light = {
+  primary: '#0396A6',
+  secondary: '#F2AD1D',
+
+  body: '#fff',
+  text: '#202020',
+  bodyRgba: '255, 255, 255',
+  textRgba: '32, 32, 32',
+
+  carouselColor: '#EEEDDE',
+
+  fontxs: 'O.75em',
+  fontsm: 'O.875em',
+  fontmd: '1em',  // 1em = 16px
+  fontlg: '1.25em',
+  fontxl: '2em',
+  fontxxl: '3em',
+  fontxxxl: '4em',
+
+  fontButton: '0.875em',
+
+  navHeight: '5em',
+}
+
+export const dark = {
+  primary: '#0396A6',
+  secondary: '#F2AD1D',
+
+  body: '#202020',
+  text: '#fff',
+  bodyRgba: '32, 32, 32',
+  textRgba: '255, 255, 255',
+
+  carouselColor: '#EEEDDE',
+
+  fontxs: 'O.75em',
+  fontsm: 'O.875em',
+  fontmd: '1em',  // 1em = 16px
+  fontlg: '1.25em',
+  fontxl: '2em',
+  fontxxl: '3em',
+  fontxxxl: '4em',
+
+  fontButton: '0.875em',
+
+  navHeight: '5em',
+}
