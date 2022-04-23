8ac71cd37c8d911c2b04c8dfd8e56e5a9d1ff83c
tony dugué <duguetony@gmail.com>
2022-04-23 17:21:41 +0200

    Team => add Wrapper & Title

--- src/components/sections/Team.js
+++ src/components/sections/Team.js
@@ -4,13 +4,28 @@ import styled from 'styled-components'
 const Team = () => {
   return (
    <Wrapper>
-    Team
+    <Title>Team</Title>
    </Wrapper>
   )
 }
 
 export default Team
 
-const Wrapper = styled.div`
+const Wrapper = styled.section`
+  min-height: 100vh;
+  width: 100%;
+  background-color: ${props => props.theme.body};
+  position: relative;
+`
 
+const Title = styled.h1`
+  font-size: ${props => props.theme.fontxxl};
+  text-transform: capitalize;
+  color: ${props => props.theme.text};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 1rem auto;
+  border-bottom: 2px solid ${props => props.theme.text};
+  width: fit-content;
 `
