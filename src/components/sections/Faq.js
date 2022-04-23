6f4a9def1b8351dc23465c5293f31752fdfdc5ed
tony dugué <duguetony@gmail.com>
2022-04-24 00:26:38 +0200

    faq => add columns

--- src/components/sections/Faq.js
+++ src/components/sections/Faq.js
@@ -4,7 +4,14 @@ import styled from 'styled-components'
 const Faq = () => {
   return (
    <Wrapper>
+
     <Title>Faq</Title>
+
+     <Container>
+       <Box>Col 1</Box>
+       <Box>Col 2</Box>
+     </Container>
+
    </Wrapper>
   )
 }
@@ -15,6 +22,7 @@ const Wrapper = styled.section`
   min-height: 100vh;
   width: 100%;
   background-color: ${props => props.theme.primary};
+  color: ${props => props.theme.body};
   position: relative;
   display: flex;
   justify-content: center;
@@ -29,4 +37,17 @@ const Title = styled.h1`
   margin: 1rem auto;
   border-bottom: 2px solid ${props => props.theme.body};
   width: fit-content;
+`
+
+const Container = styled.div`
+  width: 75%;
+  margin: 2rem auto;
+  
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const Box = styled.div`
+  width: 45%;
 `
