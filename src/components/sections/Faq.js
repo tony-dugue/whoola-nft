3d6338117a849df6ef5362489d9e5b779a657cf2
tony dugué <duguetony@gmail.com>
2022-04-24 00:17:47 +0200

    faq => add wrapper & title

--- src/components/sections/Faq.js
+++ src/components/sections/Faq.js
@@ -4,13 +4,29 @@ import styled from 'styled-components'
 const Faq = () => {
   return (
    <Wrapper>
-    Faq
+    <Title>Faq</Title>
    </Wrapper>
   )
 }
 
 export default Faq
 
-const Wrapper = styled.div`
+const Wrapper = styled.section`
+  min-height: 100vh;
+  width: 100%;
+  background-color: ${props => props.theme.primary};
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
 
+const Title = styled.h1`
+  font-size: ${props => props.theme.fontxxl};
+  text-transform: uppercase;
+  color: ${props => props.theme.body};
+  margin: 1rem auto;
+  border-bottom: 2px solid ${props => props.theme.body};
+  width: fit-content;
 `
