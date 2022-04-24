bc3ae7918544c47908bac2e489944b2398e64b29
tony dugué <duguetony@gmail.com>
2022-04-24 13:03:19 +0200

    footer => add logo & social links

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -1,11 +1,31 @@
 import React from 'react'
 import styled from 'styled-components'
 import Banner from "../Banner";
+import Logo from "../Logo";
+
+import Facebook from "../../Icons/Facebook";
+import Instagram from "../../Icons/Instagram";
+import Twitter from "../../Icons/Twitter";
+import LinkedIn from "../../Icons/LinkedIn";
 
 const Footer = () => {
   return (
    <Wrapper>
+
      <Banner />
+
+     <Container>
+      <Left>
+        <Logo />
+        <IconList>
+          <a href="http://facebook.com" target="_blank" rel="noopener"><Facebook /></a>
+          <a href="http://instagram.com" target="_blank" rel="noopener"><Instagram /></a>
+          <a href="http://twitter.com" target="_blank" rel="noopener"><Twitter /></a>
+          <a href="http://linkedin.com" target="_blank" rel="noopener"><LinkedIn /></a>
+        </IconList>
+      </Left>
+     </Container>
+
    </Wrapper>
   )
 }
@@ -22,4 +42,36 @@ const Wrapper = styled.section`
   //justify-content: center;
   //align-items: center;
   flex-direction: column;
+`
+
+const Container = styled.div`
+  width: 75%;
+  margin: 2rem auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  
+  border-bottom: 1px solid ${props => props.theme.text};
+`
+
+const Left = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const IconList = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 1rem auto;
+  
+  & > * {
+    padding-right: 0.5rem;
+    transition: all 0.2s ease;
+    
+    &:hover {
+      transform: scale(1.2);
+    }
+  }
 `
