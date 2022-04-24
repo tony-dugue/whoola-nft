a18de4200d15a699949934e0907c0aec1473d52a
tony dugué <duguetony@gmail.com>
2022-04-24 13:56:26 +0200

    footer => add menu

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -16,14 +16,28 @@ const Footer = () => {
 
      <Container>
       <Left>
+
         <Logo />
+
         <IconList>
           <a href="http://facebook.com" target="_blank" rel="noopener"><Facebook /></a>
           <a href="http://instagram.com" target="_blank" rel="noopener"><Instagram /></a>
           <a href="http://twitter.com" target="_blank" rel="noopener"><Twitter /></a>
           <a href="http://linkedin.com" target="_blank" rel="noopener"><LinkedIn /></a>
         </IconList>
+
       </Left>
+
+      <MenuItems>
+        <Item>Accueil</Item>
+        <Item>A propos</Item>
+        <Item>Roadmap</Item>
+
+        <Item>Showcase</Item>
+        <Item>Team</Item>
+        <Item>Faq</Item>
+      </MenuItems>
+
      </Container>
 
    </Wrapper>
@@ -72,6 +86,36 @@ const IconList = styled.div`
     
     &:hover {
       transform: scale(1.2);
+    }
+  }
+`
+
+const MenuItems = styled.ul`
+  list-style: none;
+  width: 50%;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  grid-gap: 1rem;
+`
+
+const Item = styled.li`
+  width: fit-content;
+  cursor: pointer;
+
+  &::after {
+    content: ' ';
+    display: block;
+    width: 0%;
+    height: 2px;
+    background: ${props => props.theme.text};
+    transition: all 0.3s ease;
+  }
+  &:hover {
+    color: ${props => props.theme.primary};
+
+    &::after {
+      width: 100%;
     }
   }
 `
